c80ce91f040db89fd47395ad5e4d378858492d72
Annasomayaji <anna.somayaji@gmail.com>
2024-01-19 14:08:39 +0000

    fifth commit, extent reports added

--- src/test/java/com/nopcommerce/demo/steps/LoginSteps.java
+++ src/test/java/com/nopcommerce/demo/steps/LoginSteps.java
@@ -2,15 +2,18 @@ package com.nopcommerce.demo.steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class LoginSteps {
     @Given("I am on homepage")
     public void iAmOnHomepage() {
         System.out.println("Hello");
+
     }
 
     @When("I click on login link")
     public void iClickOnLoginLink() {
         System.out.println("Hello again");
+        Assert.fail();
     }
 }
